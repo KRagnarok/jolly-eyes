e3b8f37ca8a466c490bf57a8acb1fb82063b61cc
sod-lol <ddssee5511@gmail.com>
2023-06-16 00:59:38 -0400

    Add more filter to exchange service

--- exchange_prober.py
+++ exchange_prober.py
@@ -1,7 +1,10 @@
+from typing import Optional
+from exchangelib.ewsdatetime import datetime
 from email_prober import BasicEmailProber, EmailAccountInfo, BasicEmailHolder, BasicEmailFilter
 from exchangelib import DELEGATE, Account, Configuration, Credentials, Q
 
 
+### Filters ###
 class BasicExchangeFilter(BasicEmailFilter):
 
     def __init__(self) -> None:
@@ -12,18 +15,60 @@ class BasicExchangeFilter(BasicEmailFilter):
     
     def get_query_function(self):
         raise NotImplementedError()
-   
+  
+class ExchangeEmailAuthorFilter(BasicExchangeFilter):
 
-### Filters ###
+    def __init__(self, author: str, contains: bool = False, like: bool = False) -> None:
+        self._author = author
+        self._contains = contains
+        self._like = like
+    
+    def get_filtered_emails(self, all_emails):
+        raise NotImplementedError()
+
+    def get_query_function(self):
+        if self._contains:
+            if self._like:
+                return Q(sender__icontains=self._author)
+            return Q(sender__contains=self._author)
+
+        if self._like:
+            return Q(sender__iexact=self._author)
+        return Q(sender=self._author)
+
+class ExchangeEmailDateFilter(BasicExchangeFilter):
+
+    def __init__(self, start_date: Optional[datetime.datetime], 
+                 end_date: Optional[datetime.datetime]) -> None:
+        self._start_date = start_date 
+        self._end_date = end_date 
+        if self._start_date == None and self._end_date == None:
+            raise Exception("At least one date should be specified")
+
+    def get_filtered_emails(self, all_emails):
+        raise NotImplementedError()
+
+    def get_query_function(self):
+        query = None
+        if self._start_date != None:
+            query = Q(start__gte=self._start_date)
+
+        if self._end_date != None:
+            if query != None:
+                query = query & Q(end__lte=self._end_date)
+            else:
+                query = Q(end__lte=self._end_date)
+        
+        return query
+        
 class ExchangeEmailSubjectFilter(BasicExchangeFilter):
-
     def __init__(self, subject: str, contains: bool = False, like: bool = False) -> None:
         self._subject = subject
         self._contains = contains
         self._like = like
 
     def get_filtered_emails(self, all_emails):
-        return all_emails.filter(subject=self._subject)
+        raise NotImplementedError()
 
     def get_query_function(self):
         if self._contains:
@@ -34,7 +79,7 @@ class ExchangeEmailSubjectFilter(BasicExchangeFilter):
         if self._like:
             return Q(subject__iexact=self._subject)
         return Q(subject=self._subject)
-
+    
 ### Holder ###
 class ExchangeEmailHolder(BasicEmailHolder):
 
--- probmail.py
+++ probmail.py
@@ -5,11 +5,11 @@ from imap_prober import *
 
 
 def main():
-    exh = IMAPEmailProber(email_acc_info=email_info)
-    # exh = ExchangeEmailProber(email_acc_info=email_info)
+    # exh = IMAPEmailProber(email_acc_info=email_info)
+    exh = ExchangeEmailProber(email_acc_info=email_info)
     exh.connect_and_auth()
     holder = exh.get_all_emails()
-    holder.apply_filter(IMAPEmailSubjectFilter(subject='instance'))
+    # holder.apply_filter(IMAPEmailSubjectFilter(subject='instance'))
     for item in holder.get_emails():
         print(item.subject)
     # for item in exh.get_new_emails():
